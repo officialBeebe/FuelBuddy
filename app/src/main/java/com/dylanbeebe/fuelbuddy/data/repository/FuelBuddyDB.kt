8638bfbb30d6ffdcdaf1dd843b8b4df1a22f8522
Dylan <dylbeebe@gmail.com>
2026-07-22 22:25:03 -0500

    Add `getDatabase()`

--- app/src/main/java/com/dylanbeebe/fuelbuddy/data/repository/FuelBuddyDB.kt
+++ app/src/main/java/com/dylanbeebe/fuelbuddy/data/repository/FuelBuddyDB.kt
@@ -1,6 +1,8 @@
 package com.dylanbeebe.fuelbuddy.data.repository
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.dylanbeebe.fuelbuddy.data.dao.MileageDAO
 import com.dylanbeebe.fuelbuddy.data.dao.UserDAO
@@ -15,4 +17,17 @@ abstract class FuelBuddyDB : RoomDatabase() {
     abstract fun vehicleDAO(): VehicleDAO
     abstract fun mileageDAO(): MileageDAO
 
+    companion object {
+        @Volatile
+        private var Instance: FuelBuddyDB? = null
+
+        fun getDatabase(context: Context): FuelBuddyDB {
+            // if the Instance is not null, return it, otherwise create a new database instance.
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, FuelBuddyDB::class.java, "item_database")
+                    .build()
+                    .also { Instance = it }
+            }
+        }
+    }
 }
