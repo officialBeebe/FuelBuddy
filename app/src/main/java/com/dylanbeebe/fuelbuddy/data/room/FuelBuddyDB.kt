542179f314c1148d505a7cfeaf83f2666b7672f9
dylan <dylbeebe@gmail.com>
2026-08-03 18:47:28 -0500

    Move FuelBuddyDB to /data/room

--- app/src/main/java/com/dylanbeebe/fuelbuddy/data/room/FuelBuddyDB.kt
+++ app/src/main/java/com/dylanbeebe/fuelbuddy/data/room/FuelBuddyDB.kt
@@ -1,19 +1,16 @@
-package com.dylanbeebe.fuelbuddy.data.repository
+package com.dylanbeebe.fuelbuddy.data.room
 
 import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.dylanbeebe.fuelbuddy.data.dao.MileageDAO
-import com.dylanbeebe.fuelbuddy.data.dao.UserDAO
 import com.dylanbeebe.fuelbuddy.data.dao.VehicleDAO
 import com.dylanbeebe.fuelbuddy.data.model.Mileage
-import com.dylanbeebe.fuelbuddy.data.model.User
 import com.dylanbeebe.fuelbuddy.data.model.Vehicle
 
-@Database(entities = [User::class, Vehicle::class, Mileage::class], version = 1)
+@Database(entities = [Vehicle::class, Mileage::class], version = 1)
 abstract class FuelBuddyDB : RoomDatabase() {
-    abstract fun userDAO(): UserDAO
     abstract fun vehicleDAO(): VehicleDAO
     abstract fun mileageDAO(): MileageDAO
 
