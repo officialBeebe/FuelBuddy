be8a4c2e29bd97f861e9fdfb0ddaeeeecb4605aa
dylan <dylbeebe@gmail.com>
2026-08-04 16:30:03 -0500

    Cleanup + note for later

--- app/src/main/java/com/dylanbeebe/fuelbuddy/MainActivity.kt
+++ app/src/main/java/com/dylanbeebe/fuelbuddy/MainActivity.kt
@@ -1,40 +1,14 @@
 package com.dylanbeebe.fuelbuddy
 
-import android.R
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.dylanbeebe.fuelbuddy.ui.VehicleViewModel
 import com.dylanbeebe.fuelbuddy.ui.theme.FuelBuddyTheme
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.dylanbeebe.fuelbuddy.data.model.Vehicle
-import com.dylanbeebe.fuelbuddy.ui.components.VehicleCard
 import com.dylanbeebe.fuelbuddy.ui.screens.HomeScreen
 
 class MainActivity : ComponentActivity() {
@@ -54,4 +28,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+// TODO: Implement Compose Navigation: https://developer.android.com/guide/navigation
 
