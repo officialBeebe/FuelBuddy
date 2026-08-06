406bfebab74821011a7e35ad04535b733f53e3ce
Dylan <dylbeebe@gmail.com>
2026-08-05 21:54:09 -0500

    Add attachments

---
+++ app/src/main/java/com/dylanbeebe/fuelbuddy/data/room/entity/MileageAttachment.kt
@@ -0,0 +1,21 @@
+package com.dylanbeebe.fuelbuddy.data.room.entity
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+import com.dylanbeebe.fuelbuddy.data.model.Mileage
+import java.util.UUID
+
+@Entity(
+    foreignKeys = [ForeignKey(
+        entity = Mileage::class,
+        parentColumns = arrayOf("mileageID"),
+        childColumns = arrayOf("mileage"),
+        onDelete = ForeignKey.CASCADE
+    )],
+)
+data class MileageAttachment(
+    @PrimaryKey val attachmentID: String = UUID.randomUUID().toString(),
+    val URI: String,
+    val mileage: String
+)
